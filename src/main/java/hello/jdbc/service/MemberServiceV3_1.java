26fedf62419c22693078c82e5b5bef7bb770cb16
parkjaehak <atom8426@naver.com>
2024-01-24 14:44:09 +0900

    featTEst

--- src/main/java/hello/jdbc/service/MemberServiceV3_1.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -23,29 +23,4 @@ public class MemberServiceV3_1 {
     private final PlatformTransactionManager transactionManager;
     private final MemberRepositoryV3 memberRepository;
 
-    public void accountTransfer(String fromId, String toId, int money){
-        // transaction start
-        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-
-        try {
-            bizLogic(fromId, toId, money); // business logic
-            transactionManager.commit(status); // 성공시 commit
-
-        } catch (Exception e) {
-            transactionManager.rollback(status); // 실패시 rollback
-            throw new IllegalStateException(e);
-        }
-    }
-
-    private void bizLogic(String fromId, String toId, int money) throws SQLException {
-        Member fromMember = memberRepository.findById(fromId);
-        Member toMember = memberRepository.findById(toId);
-
-        memberRepository.update(fromId, fromMember.getMoney() - money); // 돈을 주는 쪽
-        validation(toMember);
-        memberRepository.update(toId, toMember.getMoney() + money); // 돈을 받는 쪽
-    }
-
-
-    }
 }
